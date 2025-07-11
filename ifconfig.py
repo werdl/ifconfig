644184955da11cbc1b26948ef1379d88cd7da811
werdl <werdl@users.noreply.github.com>
2025-07-11 08:15:39 +0100

    improve

--- ifconfig.py
+++ ifconfig.py
@@ -1,23 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def process_headers():
-    info = {}
-    info['headers'] = dict(request.headers)
-    info['ip'] = request.remote_addr
-    info['method'] = request.method
-    info['form'] = request.form
-    info['port'] = request.environ.get('REMOTE_PORT')
-    info['scheme'] = request.scheme
-    info['cookies'] = request.cookies
-    info['query'] = request.query_string.decode('utf-8')
+    headers = dict(request.headers)
+    ip = request.remote_addr
+    method = request.method
+    agent = request.user_agent
+    remote_port = request.environ.get('REMOTE_PORT')
+#    scheme = request.headers['nginx-scheme']
+    mime = request.content_type
+    language = request.accept_languages
 
-    strout = json.dumps(info, indent=4)
-    print(strout)
-    return strout
+    return render_template("index.html", headers=headers, ip=ip, method=method, scheme='http', mime=mime, language=language, agent = agent)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
